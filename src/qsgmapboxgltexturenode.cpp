2d5ff5e5dff0caa4e2c833730b4397e23278a191
Rinigus <rinigus.git@gmail.com>
2025-12-30 22:51:58 +0200

    Add missing license comments and drop unused file

--- src/qsgmapboxgltexturenode.cpp
+++ src/qsgmapboxgltexturenode.cpp
@@ -1,3 +1,46 @@
+/****************************************************************************
+**
+** Based on the implementation of Mapbox GL Native QtLocation plugin at
+** https://github.com/qt/qtlocation/tree/5.9/src/plugins/geoservices/mapboxgl
+** and later versions. Developed further for integration with the plugin
+**
+** The original code license is below
+**
+** Copyright (C) 2017 The Qt Company Ltd.
+** Copyright (C) 2017 Mapbox, Inc.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the QtLocation module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL3$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPL included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include "qsgmapboxgltexturenode.h"
 #include "qsgtextureplain.h"
 
